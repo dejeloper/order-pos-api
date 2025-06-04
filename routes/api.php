52a83946a2935603e3bf2b7aa54ba00a240a09a0
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2025-06-03 22:58:37 -0500

    feat: enhance user management routes with permissions and soft delete functionality

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\RolesPermisosController;
 use App\Http\Middleware\IsUserAuth;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +18,45 @@ Route::middleware([IsUserAuth::class])->group(function () {
     Route::controller(AuthController::class)->group(function () {
         Route::post('logout', 'logout');
         Route::get('me', 'getUser');
+    });
+
+    Route::controller(RolesPermisosController::class)->group(function () {
+        // Roles
+        Route::get('/roles', 'index');
+        Route::get('/roles/{id}', 'show');
+        Route::post('/roles', 'store');
+        Route::put('/roles/{id}', 'update');
+        Route::delete('/roles/{id}', 'destroy');
+
+        // Permissions
+        Route::get('/permissions', 'permissionsIndex');
+        Route::get('/permissions/{id}', 'permissionsShow');
+        Route::post('/permissions', 'permissionsStore');
+        Route::put('/permissions/{id}', 'permissionsUpdate');
+        Route::delete('/permissions/{id}', 'permissionsDestroy');
+    });
+
+
+    Route::controller(UserController::class)->prefix('users')->group(function () {
+        // Listar
+        Route::get('/', 'index')->middleware('check_permission:view_users');
+        Route::get('/disabled', 'indexDisabled')->middleware('check_permission:view_disabled_users');
+        Route::get('/{id}', 'show')->middleware('check_permission:view_users');
+        Route::get('/trashed/{id}', 'showTrashed')->middleware('check_permission:view_trashed_users');
+        Route::get('/name/{name}', 'showByName')->middleware('check_permission:view_users_by_name');
+
+        // Crear
+        Route::post('/', 'store')->middleware('check_permission:create_users');
+
+        // Actualizar y restaurar
+        Route::patch('/{id}', 'update')->middleware('check_permission:edit_users');
+        Route::patch('/restore/{id}', 'restore')->middleware('check_permission:restore_users');
+        Route::patch('/users/{id}/permissions', 'UserController@syncPermissions')
+            ->middleware('check_permission:edit_users_permissions');
+
+        // Soft-delete y permanente
+        Route::delete('/{id}', 'destroy')->middleware('check_permission:delete_users');
+        Route::delete('/force/{id}', 'forceDelete')->middleware('check_permission:force_delete_users');
     });
 
     Route::controller(ProductController::class)->prefix('products')->group(function () {
